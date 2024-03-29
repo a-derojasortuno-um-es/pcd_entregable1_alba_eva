dcbf6d3338a3eceb8e5be92413a7a65c76f0a148
a-derojasortuno-um-es <a.derojasortuno@um.es>
2024-03-29 11:10:29 +0100

    Añadiendo el fichero entregable con todos los comandos de prueba y modificando los métodos de departamento.

--- src/entregable.py
+++ src/entregable.py
@@ -23,6 +23,9 @@ class Miembro_departamento:
     def __init__(self, departamento):
         self.departamento = departamento
     
+    def get_dep(self):
+        return self.departamento
+    
     def cambiar_dep(self, dep):
         self.departamento = dep
 
@@ -56,13 +59,6 @@ class Asignatura:
         for i in self.roles['estudiantes']:
             print(i) # pase lo que pase, si tiene un objeto o el nombre se imprimirá i.
     
-    # def __str__(self):
-    #     result = ""
-    #     result = result + "Asignatura:" + self.nombre + "con" + str(self.creditos) + "créditos," + len(self.roles["estudiantes"])  + "estudiantes y que la imparten los siguientes profesores: "
-    #     for i in self.roles["profesor"]:
-    #         result += "\n" + "Profesor:" + i.nombre # ver si se pone todos los datos del profesor o solo el nombre.
-    #     return result
-            
     def __str__(self):
         cadena = 'Asignatura: '+self.nombre+'\nCréditos: '+str(self.creditos)+'\nNº estudiantes: '+str(len(self.roles["estudiantes"]))+'\nProfesores: '
         if len(self.roles["profesor"]) == 0:
@@ -83,20 +79,24 @@ class Investigador(Persona):
         super().__init__(nombre, DNI, direccion, sexo)
         self.area = area
         self.miembro  = Miembro_departamento(departamento) # Composición.
+    
 
     def __str__(self):
-        return self.devuelveDatos() +'\nArea de investigación: '+self.area+'\nDepartamento:'+str(self.miembro.departamento)+'\n'
+        return self.devuelveDatos() +'\nArea de investigación: '+self.area+'\nDepartamento:'+str(self.miembro.get_dep())+'\n'
 
 
 class ProfesorTitular(Persona):
-    def __init__(self, nombre, DNI, direccion, sexo, area_inv, departamento, listaAsig = []):
+    def __init__(self, nombre, DNI, direccion, sexo, area_inv, departamento):
         super().__init__(nombre, DNI, direccion, sexo)
-        self.listaAsig = listaAsig
+        self.listaAsig = []
         self.rol_inv = Investigador(nombre, DNI ,direccion , sexo, area_inv, departamento)
         self.miembro  = Miembro_departamento(departamento)
 
     def add_asig(self,asig): # se implementará en la clase Universidad porque ahí estará la lista de asignaturas
         self.listaAsig.append(asig)
+    
+    def del_asig(self, asig):
+        self.listaAsig.remove(asig)
 
     def mostrar_asig(self):
         print(f"El profesor titular {self.nombre} imparte las siguientes asignaturas: ")
@@ -104,13 +104,13 @@ class ProfesorTitular(Persona):
            print(i,"\n") # se imprime el objeto Asignatura.
 
     def __str__(self):
-        return self.devuelveDatos() + '\nDepartamento:'+str(self.miembro.departamento)+'\n'
+        return self.devuelveDatos() + '\nDepartamento:'+str(self.miembro.get_dep())+'\n'
        
             
 class ProfesorAsociado(Persona):
-    def __init__(self, nombre, DNI, direccion, sexo, departamento, listaAsig = []):
+    def __init__(self, nombre, DNI, direccion, sexo, departamento):
         super().__init__(nombre, DNI, direccion, sexo)
-        self.listaAsig = listaAsig
+        self.listaAsig = []
         self.miembro  = Miembro_departamento(departamento)
 
     def add_asig(self,asig):
@@ -125,13 +125,13 @@ class ProfesorAsociado(Persona):
             print(i)
 
     def __str__(self):
-        return self.devuelveDatos() + '\nDepartamento:'+str(self.miembro.departamento)
+        return self.devuelveDatos() + '\nDepartamento:'+str(self.miembro.get_dep())
 
 
 class Estudiante(Persona):
-    def __init__(self, nombre, DNI, direccion, sexo, listaAsig = []):
+    def __init__(self, nombre, DNI, direccion, sexo):
         super().__init__(nombre, DNI, direccion, sexo)
-        self.listaAsig = listaAsig
+        self.listaAsig = []
     
     def add_asig(self,asig):
         self.listaAsig.append(asig) 
@@ -157,13 +157,11 @@ class Universidad:
         self.codpostal = codpostal
         self.ciudad = ciudad
         self._lista_inves = lista_inves # a cada lista se añaden los objetos en sí para poder recuperar sus datos.
-        self._lista_prof_tit = lista_prof_tit
+        self._lista_prof_tit = lista_prof_tit 
         self._lista_prof_asoc = lista_prof_asoc
         self._lista_est = lista_est
-        self._lista_asig = lista_asig
-
-    # se va a diferenciar a la hora de asignar asignaturas a profesores entre su rol. --- nos ahorramos recorrer dos listas.
-    # vale la pena?    
+        self._lista_asig = lista_asig 
+        self._departamentos = {}  
     
     def _buscaasig(self,nombre): # se hace un método para encontrar por el nombre de la asignatura el objeto.
         try:
@@ -224,6 +222,7 @@ class Universidad:
             i += 1
         return invest
     
+    
     def add_estudiante(self,nombre,DNI,direccion,sexo):
         est = Estudiante(nombre,DNI,direccion,sexo)
         self._lista_est.append(est)
@@ -231,15 +230,27 @@ class Universidad:
     def add_prof_titular(self,nombre,DNI,direccion,sexo,area_inv,departamento):
         prof_tit = ProfesorTitular(nombre,DNI,direccion,sexo,area_inv,departamento)
         self._lista_prof_tit.append(prof_tit)
-        self._lista_inves.append(prof_tit) # el profesor titular deberá estar, a su vez, en las dos listas. -- unificar variables.
+        self._lista_inves.append(prof_tit) # el profesor titular deberá estar, a su vez, en las dos listas. 
+        if departamento in self._departamentos: 
+            self._departamentos[str(departamento)].append(prof_tit) 
+        else:
+            self._departamentos[str(departamento)] = [prof_tit]
     
     def add_prof_asoc(self,nombre,DNI,direccion,sexo,departamento):
         prof_asoc = ProfesorAsociado(nombre,DNI,direccion,sexo,departamento)
         self._lista_prof_asoc.append(prof_asoc)
+        if departamento in self._departamentos: 
+            self._departamentos[str(departamento)].append(prof_asoc) 
+        else:
+            self._departamentos[str(departamento)] = [prof_asoc]
     
     def add_investigador(self,nombre,DNI,direccion,sexo,area,departamento):
         invest = Investigador(nombre,DNI,direccion,sexo,area,departamento)
         self._lista_inves.append(invest)
+        if departamento in self._departamentos: 
+            self._departamentos[str(departamento)].append(invest) 
+        else:
+            self._departamentos[str(departamento)] = [invest]
     
     def add_asig(self,nombre,creditos):
         for i in self._lista_asig:
@@ -263,12 +274,16 @@ class Universidad:
                 raise NotFound('Error. Este profesor no se encuentra en la base de datos') # ese nombre no corresponde a ningún profesor.
             else: # signfica que profesor es un profesor titular.
                 prof = self._buscaprofTitular(profesor)
+                dep_ant = prof.miembro.get_dep()
+                self._departamentos[str(dep_ant)].remove(prof)
                 self._lista_prof_tit.remove(prof)
                 self._lista_inves.remove(prof) # al ser una composición también se elimina.
                 for i in prof.listaAsig:
                     i.del_profesor(prof)
         else: # es un profesor asociado
             prof = self._buscaprofAsoc(profesor)
+            dep_ant = prof.miembro.get_dep()
+            self._departamentos[str(dep_ant)].remove(prof)
             self._lista_prof_asoc.remove(prof)
             for i in prof.listaAsig:
                 i.del_profesor(prof)
@@ -277,6 +292,8 @@ class Universidad:
         try:
             invest = self._buscainvest(investigador)
             self._lista_inves.remove(invest)
+            dep_ant = invest.miembro.get_dep()
+            self._departamentos[str(dep_ant)].remove(invest)
             if invest in self._lista_prof_tit:
                 self._lista_prof_tit.remove(invest) # se elimina también de la lista de profesores titulares.
         except:
@@ -285,22 +302,40 @@ class Universidad:
 
     def cambiar_dep(self,nombre, dep):
         assert dep == Departamento.DIIC or dep == Departamento.DIS or dep == Departamento.DITEC , 'Nombre de departamento inválido.'
-        try:
-            p = self._buscaprofAsoc(nombre)
-            if p != None:
-                p.miembro.cambiar_dep(dep)
+        p = self._buscaprofAsoc(nombre)
+        if p != None:
+            dep_antiguo = p.miembro.get_dep()
+            self._departamentos[str(dep_antiguo)].remove(p)
+            p.miembro.cambiar_dep(dep)
+            if str(dep) in self._departamentos:
+                self._departamentos[str(dep)].append(p)
             else:
-                pt = self._buscaprofTitular(nombre)
-                if pt != None:
-                    pt.miembro.cambiar_dep(dep)
-                    inv = self._buscainvest(nombre) #todos los prof titulares son tmb investigadores
-                    inv.miembro.cambiar_dep(dep)
+                self._departamentos[str(dep)] = [p]
+        else:
+            pt = self._buscaprofTitular(nombre)
+            if pt != None:
+                dep_antiguo = pt.miembro.get_dep()
+                self._departamentos[str(dep_antiguo)].remove(pt)
+                pt.miembro.cambiar_dep(dep)
+                inv = self._buscainvest(nombre) 
+                inv.miembro.cambiar_dep(dep)
+                if str(dep) in self._departamentos:
+                    self._departamentos[str(dep)].append(pt)
                 else:
-                    i = self._buscainvest(nombre)
-                    if self._buscainvest(nombre) != None: #investigadores q no son prof titulares
-                        i.miembro.cambiar_dep(dep)
-        except:
-            raise NotFound('Error. Esta persona no se encuentra en la base de datos.')
+                    self._departamentos[str(dep)] = [pt]
+            else:
+                i = self._buscainvest(nombre)
+                if self._buscainvest(nombre) != None:
+                    dep_antiguo = i.miembro.get_dep()
+                    self._departamentos[dep_antiguo].remove(i) 
+                    i.miembro.cambiar_dep(dep)
+                    if str(dep) in self._departamentos:
+                        self._departamentos[str(dep)].append(i)
+                    else:
+                        self._departamentos[str(dep)] = [i]
+                else:
+                    raise NotFound('Error. Esta persona no se encuentra en la base de datos.')
+
 
             
     def asignar_asig_est(self,estudiante,nom_asig):
@@ -314,7 +349,7 @@ class Universidad:
     def del_asig_est(self, estudiante, nom_asig):
         asig = self._buscaasig(nom_asig)
         est = self._buscaestud(estudiante)
-        if asig  not in est.listaAsig:
+        if asig not in est.listaAsig:
             raise NotFound('Error. Esta asignatura no se encuentra entre las asignaturas del estudiante.')
         est.del_asig(asig)
         asig.del_estudiante(est)
@@ -355,11 +390,11 @@ class Universidad:
             prof_asoc.del_asig(asig)
             asig.del_profesot(prof_asoc)
         
-    def mostrar_lista_asig_est(self,estudiante): 
+    def mostrar_asign_estudiante(self,estudiante): 
         est = self._buscaestud(estudiante)
         est.mostrar_asig()
 
-    def mostrar_lista_asig_prof(self,profesor): 
+    def mostrar_asign_profesor(self,profesor): 
         prof_asoc = self._buscaprofAsoc(profesor)
         if prof_asoc == None:
             prof_titular = self._buscaprofTitular(profesor)
@@ -370,67 +405,102 @@ class Universidad:
         else: # es un profesor asociado
             prof_asoc.mostrar_asig()
     
-    def mostrar_lista_asig_totales(self):
+    def mostrar_dep(self,dep):
+        try:
+            print(f"--MIEMBROS DEL DEPARTAMENTO {str(dep)} --")
+            for i in self._departamentos[str(dep)]:
+                print(i)
+        except:
+            raise NotFound('No se encuentra este departamento.')
+
+    def mostrar_asignaturas(self):
         print("La Universidad",self.nombre,"imparte las siguientes asignaturas: ")
         for i in self._lista_asig:
             print(i,"\n")
     
-    def mostrar_lista_est_asig(self, nom_asig):
+    def mostrar_estudiantes_asign(self, nom_asig):
         a = uni._buscaasig(nom_asig)
         a.lista_estudiantes()
-
-    def get_estudiantes(self):
+    
+    def mostrar_profesores(self):
+        print("--PROFESORES ASOCIADOS--")
+        for asoc in self._lista_prof_asoc:
+            print(asoc,"\n")
+        print("--PROFESORES TITULARES--")
+        for tit in self._lista_prof_tit:
+            print(tit)
+    
+    def mostrar_estudiantes(self):
+        print("--LISTADO ESTUDIANTES--")
         for est in self._lista_est:
-            print(est)
+            print(est,"\n")
     
-    def get_prof_tit(self):
-        for prof in self._lista_prof_tit:
-            print(prof)
+    def mostrar_investigadores(self):
+        print("--LISTADO INVESTIGADORES--")
+        for inv in self._lista_inves:
+            print(inv,"\n")
     
-    def get_prof_asoc(self):
-        for prof in self._lista_prof_asoc:
-            print(prof)
-
-    def get_inves(self):
-        for i in self._lista_inves:
-            print(i)
         
 if __name__ == "__main__":
     uni = Universidad('UMU',30840,'Murcia')
-    uni.add_asig('mates', 6)
-    uni.add_asig('fisica',4)
-    #uni.add_asig('mates',8)
-    uni.add_asig('lengua',8)
+    uni.add_asig('Matemáticas', 6)
+    uni.add_asig('Física',4)
+    #uni.add_asig('Matemáticas',8) # saldrá error por repetir una asignatura ya creada.
+    uni.add_asig('Lengua',8)
+    uni.add_asig('Historia',6)
+    uni.mostrar_asignaturas()
 
-    uni.add_prof_asoc('Ana','123456789','ygfadgu','F',Departamento.DIIC)
-    uni.asignar_asig_prof('Ana','mates')
+    uni.add_prof_asoc('Ana Martínez','12345678A','Murcia','F',Departamento.DIIC)
+    uni.asignar_asig_prof('Ana Martínez','Lengua')
+
+    uni.add_prof_asoc('Juan Fernández','12121212B','Madrid','M',Departamento.DIS)
+    uni.asignar_asig_prof('Juan Fernández','Física')
+    uni.asignar_asig_prof('Juan Fernández','Matemáticas')
+
+    uni.mostrar_asign_profesor('Juan Fernández')
     
-    pt = ProfesorTitular('Juan','162435678','hgvahgv','M','x',Departamento.DIIC)
+    uni.add_prof_titular('Pepe Ruiz','90091234C','León','M','Área de Computadores',Departamento.DITEC)
+    uni.asignar_asig_prof('Pepe Ruiz','Matemáticas')
+    uni.asignar_asig_prof('Pepe Ruiz','Historia')
     
-    uni.add_estudiante('Mar', '571867890', 'fsgsh', 'F')
-    uni.add_estudiante('Emma', '571867890', 'fsgsh', 'F')
-    uni.add_estudiante('Sara', '571867890', 'fsgsh', 'F')
+    uni.mostrar_profesores()
 
+    uni.cambiar_dep('Pepe Ruiz',Departamento.DIIC)
 
-    uni.asignar_asig_est('Emma','mates')
-    uni.asignar_asig_est('Mar','lengua')
-    uni.mostrar_lista_asig_est('Emma')
-    uni.mostrar_lista_est_asig('lengua')
-    uni.mostrar_lista_est_asig('fisica')
+    uni.mostrar_dep(Departamento.DIIC)
 
-    uni.del_asig_est('Emma','mates')
-    #uni.asignar_asig_est('Eva','fisica')
-    uni.mostrar_lista_est_asig('mates')
+    uni.mostrar_investigadores()
 
-    uni.add_prof_asoc('pepe','123456789','bhsb','M',Departamento.DIIC)
-    #uni.add_prof_asoc('jose','gsh','hsg','b',Departamento.DITEC)
-    uni.add_prof_titular('rosa','896754145','bvsj','F','hhs',Departamento.DIS)
-    uni.add_prof_titular('jose','871956432','hgca','M','bjwh',Departamento.DIIC)
-    uni.cambiar_dep('rosa',Departamento.DITEC)
+    uni.mostrar_asignaturas()
     
-    #uni.get_prof_asoc()
-    #uni.get_prof_tit()
-    uni.mostrar_lista_asig_totales()
-    uni.mostrar_lista_est_asig('lengua')
+    #uni.add_estudiante('Mar Soler', '571867890L', 'Valladolid', 'F') # tendrá un error de DNI por tener una longitud de 10, en vez de 9.
+
+    uni.add_estudiante('Manuel Rodríguez', '87654321M', 'Valladolid', 'F')
+    uni.asignar_asig_est('Manuel Rodríguez','Matemáticas')
+    uni.asignar_asig_est('Manuel Rodríguez','Física')
+
+    uni.add_estudiante('Emma García', '92302020P', 'Barcelona', 'F')
+    uni.asignar_asig_est('Emma García','Historia')
+    uni.asignar_asig_est('Emma García','Lengua')
+
+    uni.add_estudiante('Jose Domínguez', '22222222M', 'Bilbao', 'M')
+    #uni.asignar_asig_est('Jose Domínguez','Cálculo') # dará fallo porque la asignatura no se encontrará
+    uni.asignar_asig_est('Jose Domínguez','Física')
+
+    uni.mostrar_estudiantes()
+
+    uni.mostrar_asign_estudiante('Emma García')
+
+    uni.del_asig_est('Emma García','Lengua')
+
+    uni.mostrar_asign_estudiante('Emma García')
+
+    uni.del_estudiante('Jose Domínguez')
+
+    uni.mostrar_estudiantes()
+    
+    uni.mostrar_estudiantes_asign('Física')
+
+
 
 
